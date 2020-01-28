5337b21d29387168437fda13d3a6fae20ecac46a
RevathiNarra <59167586+RevathiNarra@users.noreply.github.com>
2020-01-28 16:40:41 +0530

    Add files via upload

---
+++ P11.java
@@ -0,0 +1,6 @@
+class P11{
+public static void main(String args[]){
+   System.out.print(args[0] + " technologies ");
+  System.out.println(args[1]);
+  }
+}
---
+++ P12.java
@@ -0,0 +1,5 @@
+class P12{
+  public static void main(String args[]){
+  System.out.println("Welcome "+args[0]);
+ }
+}
